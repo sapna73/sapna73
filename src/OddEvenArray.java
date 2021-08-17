3a39ba5cde7e7829b687734523e809bc23dc0539
sapna73 <sg.sapna73@gmail.com>
2021-08-17 14:41:56 +0530

    Even Odd numbers

--- src/OddEvenArray.java
+++ src/OddEvenArray.java
@@ -1,6 +1,7 @@
 public class OddEvenArray {
     public static void main(String args[]){
-        int a[] = {1,2,5,6,3,2};
+
+        int a[] = {1,2,5,6,3,2,7,9};
         System.out.println("Odd Numbers:");
         for(int i=0; i<a.length; i++){
             if(a[i]%2 != 0){
