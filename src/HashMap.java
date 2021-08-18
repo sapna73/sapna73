765a8d5e566f4cff3850977cbe767fa3381d72b4
sapna73 <sg.sapna73@gmail.com>
2021-08-18 17:07:56 +0530

    Hash maps

---
+++ src/HashMap.java
@@ -0,0 +1,13 @@
+public class HashMap {
+    public static void main(String[] args){
+        java.util.HashMap<String, String> capitalCities = new java.util.HashMap<String, String>();
+
+        capitalCities.put("India", "New Delhi");
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+        System.out.println(capitalCities);
+
+    }
+}
