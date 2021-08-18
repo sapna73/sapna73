ca3e31dbb65b5777aca9186276e9daf09e7a2d02
sapna73 <sg.sapna73@gmail.com>
2021-08-18 13:49:13 +0530

    Linked list

---
+++ src/LinkedListAdd.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class LinkedListAdd {
+    public static void main(String[] args){
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("BMW");
+        cars.add("FORD");
+        cars.add("KIA");
+        cars.add("VOLVO");
+        System.out.println(cars);
+    }
+}
